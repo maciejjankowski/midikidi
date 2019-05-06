b4e3fef3a75df3dc8cf1d9f64de654da308a1d8c
mj <maj1337+git@gmail.com>
2019-05-06 09:05:29 +0200

    sample dump

---
+++ sample dump.py
@@ -0,0 +1,22 @@
+import time
+import rtmidi
+
+NOTES = [36, 38, 40, 41, 43, 45]
+
+
+def program_change(program_number):
+  pass
+
+def play_sample(sample_number):
+  for velocity in [15, 31, 63, 95, 111, 127]
+    play_note(NOTES[sample_number], velocity)
+    time.sleep(5)
+
+def main():
+  for program in range(0, 800):
+    program_change(program)
+    for sample in range(6):
+      play_sample(sample)
+
+if __name__ == '__main__':
+    main()
